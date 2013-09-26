fbbb21431d42329b86a42d1e25c345e6e0274f59
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2013-09-26 00:27:58 -0500

    Version que sacamos Domix y yo

--- tic_tac_toe/tictactoe.hs
+++ tic_tac_toe/tictactoe.hs
@@ -1,5 +1,10 @@
 
+import Data.List.Split as S
+
 type Board = [[Int]]
+
+newBoard :: Board
+newBoard = [[0,0,0],[0,0,0],[0,0,0]]
 
 value :: Board -> Int -> Int -> Int
 value board row column = board !! row !! column
@@ -7,6 +12,7 @@ value board row column = board !! row !! column
 isValidMovement :: Board -> Int -> Int -> Bool
 isValidMovement board row column = value board row column  == 0 
 
+setMovement :: Board -> Int -> Int -> Int -> Board
 setMovement board row column player = (take row board) ++ [(substitute (board !! row) column player)] ++ drop (row + 1) board
                                where substitute row column player = (take column row) ++ [player] ++ drop (column + 1) row
 
@@ -21,5 +27,21 @@ winByDiag board player = any (\row -> all (== player) row) [[value board 0 0, va
 
 isWinner :: Board -> Int -> Bool
 isWinner board player = winByRow board player || winByColumn board player || winByDiag board player
+
+getMovement :: IO (Int, Int)
+getMovement = do line <- getLine
+                 let (row:col:[]) = S.splitOn " " line
+                 return (read row ::Int, read col :: Int)
+
+nuevaFuncion uno dos =  uno + dos 
+
+--play :: IO ()
+--play player = do movement <- getMovement
+--                 putStrLn "movement for " ++ show player
+--                 play (if player == 1 then 2 else 1) 
+
+--main :: IO ()
+--main = do let board = newBoard
+--          play 1
 
 
