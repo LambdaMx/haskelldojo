a13b654eb1891352afb94e25d6fda9990d8b8db3
Agustin Ramos <agustin.ramos@knowtion.biz>
2013-10-01 12:33:29 -0500

    Mejora de la interfaz

--- session_4/tictactoe/domix_agus/tictactoe.hs
+++ session_4/tictactoe/domix_agus/tictactoe.hs
@@ -10,7 +10,9 @@ newBoard :: Board
 newBoard = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
 
 showBoard :: Board -> IO()
-showBoard board = mapM_ putStrLn $ intersperse "-----" $ map (\r -> intersperse '|' r) board 
+showBoard board = do putStrLn ""
+                     mapM_ putStrLn $ intersperse "-----" $ map (\r -> intersperse '|' r) board 
+                     putStrLn ""
 
 val :: Board -> Int -> Int -> Char
 val board row col = board !! row !! col
@@ -35,9 +37,9 @@ doMovement board row col player
     where substitute row col player = (take col row) ++ [sym player] ++ drop (col + 1) row
 
 isWinner :: Board -> Int -> Bool
-isWinner board player           = winByRow  board player || 
-                                  winByCol  board player ||
-                                  winByDiag board player
+isWinner board player              = winByRow  board player || 
+                                     winByCol  board player ||
+                                     winByDiag board player
   where winByRow  board player     = anyRowEqualTo board             player
         winByCol  board player     = anyRowEqualTo (transpose board) player
         winByDiag board player     = anyRowEqualTo diagLines         player
@@ -46,27 +48,36 @@ isWinner board player           = winByRow  board player ||
         diagLines                  = [[val board 0 0, val board 1 1, val board 2 2], 
                                       [val board 0 2, val board 1 1, val board 2 0]]
 
-getMovement :: Int -> IO (Int, Int)
-getMovement player = do putStrLn $ "Your turn player " ++ show player ++ ":"
-                        line <- getLine
-                        if correctInput line
-                        then do let (row:col:[]) = Split.splitOn "," line
-                                return (read row ::Int, read col :: Int)
-                        else do putStrLn "Wrong movement! Try again:"
-                                getMovement player
-  where correctInput input = isJust $ matchRegex (mkRegex "^[012],[012]$") input
+readMovement :: Int -> (String,String) -> IO (Int, Int)
+readMovement player names = do putStrLn $ "\nYour turn " ++ nameOf player ++ ":"
+                               line <- getLine
+                               if correctInput line
+                               then do let (row:col:[]) = Split.splitOn "," line
+                                       return ((read row ::Int)-1, (read col :: Int)-1)
+                               else do putStrLn "\nWrong movement! Try again."
+                                       readMovement player names
+  where correctInput input = isJust $ matchRegex (mkRegex "^[123],[123]$") input
+        nameOf player      = if player == 1 then fst names else snd names
 
-play :: Board -> Int -> IO ()
-play board player = do showBoard board
-                       movement <- getMovement player
-                       let updatedBoard = doMovement board (fst movement) (snd movement) player
-                       case updatedBoard of
-                         Just board -> if isWinner board player 
-                                       then do showBoard board
-                                               putStrLn $ "You won player " ++ show player ++ " !!!"
-                                               return ()
-                                       else play board $ otherPlayer player
-                         Nothing    -> do putStrLn "Wrong movement. Try again !!!"
-                                          play board player
+play :: Board -> (String,String) -> Int -> IO (Int)
+play board names player = do showBoard board
+                             movement <- readMovement player names
+                             let updatedBoard = doMovement board (fst movement) (snd movement) player
+                             case updatedBoard of
+                               Just board -> if isWinner board player 
+                                             then do showBoard board
+                                                     return (player)
+                                             else play board names $ otherPlayer player
+                               Nothing    -> do putStrLn "\nThat position is already used! Try again."
+                                                play board names player
+
 main :: IO ()
-main = do play newBoard 1 
+main = do putStrLn "Name of the first player: " 
+          p1Name <- getLine
+          putStrLn "Name of the second player: " 
+          p2Name <- getLine
+          result <- play newBoard (p1Name, p2Name) 1 
+          case result of
+            0 -> putStrLn "\nIt's a draw ... come on! It's just tic-tac-toe!"
+            1 -> putStrLn $ "\nYou won " ++ p1Name ++ " !!!"
+            2 -> putStrLn $ "\nYou won " ++ p2Name ++ " !!!"
